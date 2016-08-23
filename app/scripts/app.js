26a15298d8e9aa58bdb2b843efc4e6cd66304e40
Squaq <squaq@Squaq-Mac.local>
2016-08-22 23:30:54 -0300

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name calcCApp
+ * @description
+ * # calcCApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('calcCApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name calcCApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the calcCApp
+ */
+angular.module('calcCApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
